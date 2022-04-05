e86160359faa3a4083f17b6a527057854687b773
Archit Patel <architpatel07@icloud.com>
2022-04-05 00:48:17 -0400

    Created ViewModel to load Json file

--- Guidomia/Guidomia/ViewModel/CarViewModel.swift
+++ Guidomia/Guidomia/ViewModel/CarViewModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+struct CarViewModel {
+    public var data: [Cars]?
+
+    init() {
+        data = loadJson(forName: "car_list")
+    }
+    
+    func loadJson(forName name: String) -> [Cars]? {
+        if let url = Bundle.main.url(forResource: name, withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                let jsonData = try decoder.decode([Cars].self, from: data)
+                return jsonData
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        return nil
+    }
+}
